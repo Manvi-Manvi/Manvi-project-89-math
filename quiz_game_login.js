d32f3a26034eeac2696bd7e9f2c02dc1e7d1839a
Manvi-Manvi <86973973+Manvi-Manvi@users.noreply.github.com>
2021-10-28 09:50:46 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function Login(){
+    player1Name=document.getElementById("player1_name_input").value;
+    player2Name=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name", player1Name);
+    localStorage.setItem("player2_name", player2Name);
+    window.location.replace("game_page.html");
+    
+}
